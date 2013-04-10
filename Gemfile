2baaeb7252cb5ff1593e6c51830f844669dc1b7f
Thiago Pinto <tapgyn@gmail.com>
2013-04-10 01:22:13 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.0.beta1'
+
+gem 'pg'
+gem 'jquery-rails'
+gem 'turbolinks'            # https://github.com/rails/turbolinks
+gem 'jbuilder', '~> 1.0.1'  # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 4.0.0.beta1'
+  gem 'coffee-rails', '~> 4.0.0.beta1'
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ex3::Application.config.secret_key_base = 'cf3da036009f0ec4220f3996ef7f79617fdce01f577117e0a21d074a3d2490eeaa983337f07859d048780d4fdee50bfce22ac2f87e27a5d54c68f9ac4d4d7059'
