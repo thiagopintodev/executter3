5180480576708b2d77856087d40f520668686817
Thiago Pinto <tapgyn@gmail.com>
2013-05-05 01:37:54 -0400

    secret token goes to settings.yml

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Ex3::Application.config.secret_key_base = 'cf3da036009f0ec4220f3996ef7f79617fdce01f577117e0a21d074a3d2490eeaa983337f07859d048780d4fdee50bfce22ac2f87e27a5d54c68f9ac4d4d7059'
+Ex3::Application.config.secret_key_base = Settings.env.session_secret
