ff88c2dcef689f6a0cb9edd0d94e77bd03dfe7b2
Thiago Pinto <tapgyn@gmail.com>
2013-06-06 11:15:48 -0400

    edit migration, empty gender becomes 'male'

--- db/migrate/20130427171412_rename_to_gender_and_born_on.rb
+++ db/migrate/20130427171412_rename_to_gender_and_born_on.rb
@@ -1,6 +1,7 @@
 class RenameToGenderAndBornOn < ActiveRecord::Migration
   def up
     add_column :users, :gender, :string
+    User.where(sex: nil).update_all(gender: :male)
     User.where(sex: 0).update_all(gender: :male)
     User.where(sex: 1).update_all(gender: :female)
     remove_column :users, :sex
