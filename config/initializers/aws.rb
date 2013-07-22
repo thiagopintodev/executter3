40bf811052a39ca35f6b951e500c217cb1abf8ce
Thiago Pinto <tapgyn@gmail.com>
2013-07-21 22:47:20 -0400

    ensuring initializer for aws

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,5 +1,6 @@
 if Settings.env.aws.access_key_id.present?
 
-  AWS.config(Settings.env.aws)
+  s = Settings.env.aws
+  AWS.config(access_key_id: s.access_key_id, secret_access_key: s.secret_access_key)
 
 end
